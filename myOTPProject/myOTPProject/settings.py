0d33ab75085fc1b284159d3e708d74616c15f41f
moshfequr <moshfequrrahman57@gmail.com>
2026-04-13 15:57:35 +0600

    static_url change to 'otpservice/static/'

--- myOTPProject/myOTPProject/settings.py
+++ myOTPProject/myOTPProject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'otpservice/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "myPdf" / "static",
 ]
