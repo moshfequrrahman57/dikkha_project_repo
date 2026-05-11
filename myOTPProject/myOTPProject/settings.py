5d5c736a9feca3fd7f6f9b0a9608e06120813d72
moshfequr <moshfequrrahman57@gmail.com>
2026-05-11 21:04:09 +0600

    brevo smtp config done

--- myOTPProject/myOTPProject/settings.py
+++ myOTPProject/myOTPProject/settings.py
@@ -177,11 +177,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #email backend configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'mail.dikkha.info'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'banbeis@dikkha.info'
+# EMAIL_HOST = 'mail.dikkha.info'
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'banbeis@dikkha.info'
+EMAIL_HOST_USER= 'a5b5a3001@smtp-brevo.com'
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
